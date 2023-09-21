d077d3babbc58a9809176eb6d83359c12b03d2e8
Evgeny <transingularity@gmail.com>
2023-09-21 11:00:54 +0300

    Initial commit

---
+++ src/main/java/ru/dankoy/korvotoanki/KorvoToAnkiApplication.java
@@ -0,0 +1,13 @@
+package ru.dankoy.korvotoanki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorvoToAnkiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KorvoToAnkiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/dankoy/korvotoanki/KorvoToAnkiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dankoy.korvotoanki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorvoToAnkiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
