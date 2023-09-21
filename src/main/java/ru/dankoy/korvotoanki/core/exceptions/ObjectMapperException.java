83e6ae542f465051a90f6a088ec060e6b89bc03d
Evgeny <transingularity@gmail.com>
2023-09-21 14:05:15 +0300

    chore: added exceptions

---
+++ src/main/java/ru/dankoy/korvotoanki/core/exceptions/ObjectMapperException.java
@@ -0,0 +1,9 @@
+package ru.dankoy.korvotoanki.core.exceptions;
+
+public class ObjectMapperException extends KorvoRootException {
+
+  public ObjectMapperException(String message, Exception e) {
+    super(message, e);
+  }
+
+}
