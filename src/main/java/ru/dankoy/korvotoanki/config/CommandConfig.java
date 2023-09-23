d4a8de7d588bf5fb3b773946349cf34905b89a09
Evgeny <transingularity@gmail.com>
2023-09-23 12:44:48 +0300

    Google translator connector (#7)
    * chore: added okhttp dependency
    
    * chore: added default app properties
    
    * refactor: added config classes
    
    * refactor: added google translator connector
    
    * refactor: added google translator exception
    
    * refactor: added google translator shell command
    
    * refactor: added comment

---
+++ src/main/java/ru/dankoy/korvotoanki/config/CommandConfig.java
@@ -0,0 +1,11 @@
+package ru.dankoy.korvotoanki.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.command.annotation.CommandScan;
+
+@Configuration
+@CommandScan(value = "ru.dankoy.korvotoanki.core.command")
+public class CommandConfig {
+
+}
---
+++ src/main/java/ru/dankoy/korvotoanki/config/OkHttpConfig.java
@@ -0,0 +1,68 @@
+package ru.dankoy.korvotoanki.config;
+
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import okhttp3.ConnectionPool;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class OkHttpConfig {
+
+  private final DebugProperties debugProperties;
+
+  @SuppressWarnings("KotlinInternalInJava")
+  @Bean
+  public OkHttpClient okHttpClient() {
+
+    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    httpClient.connectionPool(new ConnectionPool(10, 5, TimeUnit.SECONDS));
+    httpClient.callTimeout(10L, TimeUnit.SECONDS);
+
+    if (debugProperties.isDebug()) {
+      var interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      httpClient.interceptors().add(
+          interceptor
+      );
+    }
+
+    httpClient.interceptors().add(
+        new UserAgentInterceptor("Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+    );
+
+    return httpClient.build();
+
+  }
+
+
+  static class UserAgentInterceptor implements Interceptor {
+
+    private final String userAgent;
+
+    public UserAgentInterceptor(String userAgent) {
+      this.userAgent = userAgent;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+      Request originalRequest = chain.request();
+      Request requestWithUserAgent = originalRequest.newBuilder()
+          .removeHeader("User-Agent")
+          .addHeader("User-Agent", userAgent)
+          .build();
+      return chain.proceed(requestWithUserAgent);
+    }
+
+  }
+
+}
