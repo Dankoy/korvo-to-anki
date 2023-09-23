6e5cc16dc50b6510bb7abd39c8c5bc5b202c6a69
Evgeny <transingularity@gmail.com>
2023-09-23 12:34:53 +0300

    refactor: added google translator exception

---
+++ src/main/java/ru/dankoy/korvotoanki/core/exceptions/GoogleTranslatorException.java
@@ -0,0 +1,13 @@
+package ru.dankoy.korvotoanki.core.exceptions;
+
+public class GoogleTranslatorException extends KorvoRootException {
+
+  public GoogleTranslatorException(Exception e) {
+    super(e);
+  }
+
+  public GoogleTranslatorException(String message, Exception e) {
+    super(message, e);
+  }
+
+}
