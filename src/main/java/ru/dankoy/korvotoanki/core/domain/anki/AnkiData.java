cb5cbdebd51fad1da5020cbe85d6f9fba8cede54
Evgeny <transingularity@gmail.com>
2023-09-30 14:00:58 +0300

    refactor:  added anki data domain

---
+++ src/main/java/ru/dankoy/korvotoanki/core/domain/anki/AnkiData.java
@@ -0,0 +1,21 @@
+package ru.dankoy.korvotoanki.core.domain.anki;
+
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+public class AnkiData {
+
+  private final String word;
+  private final String myExample;
+  private final List<String> translations; // from google translate
+  private final String transcription; // from dictionaryapi, if not found then from Google Translate or null
+  private final List<Meaning> meanings; // from dictionaryapi, if not found then from Google Translate or empty
+  private final String book;
+
+}
---
+++ src/main/java/ru/dankoy/korvotoanki/core/domain/anki/Meaning.java
@@ -0,0 +1,17 @@
+package ru.dankoy.korvotoanki.core.domain.anki;
+
+import java.util.List;
+
+/**
+ * @param type noun, verb
+ * @param definitions actual definitions
+ * @param synonyms
+ * @param antonyms
+ */
+public record Meaning(
+    String type,
+    List<Definition> definitions,
+    List<String> synonyms,
+    List<String> antonyms) {
+
+}
