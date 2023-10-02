007762b5b137bd7d9730a7b8c536e152a6e7d8e9
Evgeny <transingularity@gmail.com>
2023-10-02 15:40:31 +0300

    refactor: added state dto

---
+++ src/main/java/ru/dankoy/korvotoanki/core/dto/State.java
@@ -0,0 +1,15 @@
+package ru.dankoy.korvotoanki.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class State {
+
+  private String word;
+
+}
