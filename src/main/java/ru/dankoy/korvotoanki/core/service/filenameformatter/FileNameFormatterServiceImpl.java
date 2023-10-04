0b9d26b7fdd592d9d631b7e1e26c71ebd0311d3f
Evgeny <transingularity@gmail.com>
2023-10-04 12:18:46 +0300

    fix: filename formatter uses date time provider as singleton bean

--- src/main/java/ru/dankoy/korvotoanki/core/service/filenameformatter/FileNameFormatterServiceImpl.java
+++ src/main/java/ru/dankoy/korvotoanki/core/service/filenameformatter/FileNameFormatterServiceImpl.java
@@ -1,24 +1,17 @@
 package ru.dankoy.korvotoanki.core.service.filenameformatter;
 
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Lookup;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.dankoy.korvotoanki.core.service.datetimeprovider.DateTimeProvider;
 
 @Component
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class FileNameFormatterServiceImpl implements FileNameFormatterService {
 
-  // Prototype, creates every time
-  @Lookup
-  public DateTimeProvider getDateTimeProvider() {
-    return null;
-  }
+  private final DateTimeProvider dateTimeProvider;
 
   @Override
   public String format(String fileName) {
-
-    var dateTimeProvider = getDateTimeProvider();
 
     return fileName + "-" + dateTimeProvider.now() + ".txt";
   }
