1845fe20a4b809bf91c22a8e03e2a5abb9462b77
Evgeny <transingularity@gmail.com>
2023-10-04 11:55:27 +0300

    refacto: DateTimeProviderImpl is singleton

--- src/main/java/ru/dankoy/korvotoanki/core/service/datetimeprovider/DateTimeProviderImpl.java
+++ src/main/java/ru/dankoy/korvotoanki/core/service/datetimeprovider/DateTimeProviderImpl.java
@@ -3,13 +3,10 @@ package ru.dankoy.korvotoanki.core.service.datetimeprovider;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @RequiredArgsConstructor
 public class DateTimeProviderImpl implements DateTimeProvider {
 
