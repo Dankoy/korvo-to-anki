2485eb73a903b1f99dfe5611e55b22027bd20094
Evgeny <transingularity@gmail.com>
2024-06-16 19:57:53 +0300

    refactor: updated migrations

--- src/main/java/ru/dankoy/korvotoanki/migration/flyway/V2__MigrateFileStateToSqlite.java
+++ src/main/java/ru/dankoy/korvotoanki/migration/flyway/V2__MigrateFileStateToSqlite.java
@@ -1,7 +1,8 @@
 package ru.dankoy.korvotoanki.migration.flyway;
 
-import java.sql.PreparedStatement;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
@@ -16,6 +17,8 @@ import ru.dankoy.korvotoanki.core.service.state.StateService;
 @Slf4j
 public class V2__MigrateFileStateToSqlite extends BaseJavaMigration {
 
+  private static final int BATCH_SIZE = 100;
+
   private final StateService fileStateService;
 
   @Override
@@ -25,35 +28,38 @@ public class V2__MigrateFileStateToSqlite extends BaseJavaMigration {
 
     if (!currentState.isEmpty()) {
 
+      Collection<List<State>> batches =
+          currentState.stream()
+              .collect(Collectors.groupingBy(it -> currentState.indexOf(it) / BATCH_SIZE))
+              .values();
+
       var connection = context.getConnection();
 
       connection.setAutoCommit(false);
 
-      try (PreparedStatement ps =
-          connection.prepareStatement("insert into state(word) values (?)")) {
+      try (var ps = connection.prepareStatement("insert into state(word) values (?)")) {
 
-        int count = 0;
+        for (List<State> batch : batches) {
 
-        for (State elem : currentState) {
+          for (State state : batch) {
 
-          ps.setString(1, elem.word());
-          ps.addBatch();
-
-          count++;
-
-          // batch every 100 elements
-          if (count % 100 == 0) {
-            int[] numUpdates = ps.executeBatch();
-
-            for (int i = 0; i < numUpdates.length; i++) {
-              if (numUpdates[i] == -2) log.error("Execution {}: unknown number of rows updated", i);
-              else log.debug("Execution {} successful: {} rows updated", i, numUpdates[i]);
-            }
+            ps.setString(1, state.word());
+            ps.addBatch();
           }
 
-          connection.commit();
+          // batch every 100 elements
+          int[] numUpdates = ps.executeBatch();
+
+          for (int i = 0; i < numUpdates.length; i++) {
+            if (numUpdates[i] == -2) log.error("Execution {}: unknown number of rows updated", i);
+            else log.debug("Execution {} successful: {} rows updated", i, numUpdates[i]);
+          }
         }
+
+        connection.commit();
       }
+    } else {
+      log.info("No state in file to migrate from");
     }
   }
 }
--- src/main/resources/db/migration/1.0/V1__init_schema.sql
+++ src/main/resources/db/migration/1.0/V1__init_schema.sql
@@ -3,6 +3,6 @@ drop table if exists state;
 create table state
 (
     id      integer primary key autoincrement,
-    word    varchar(60),
+    word    varchar(60) not null,
     created timestamp not null default current_timestamp
 );
