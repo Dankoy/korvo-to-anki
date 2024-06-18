bc438307496549fd4c2fe0212c3f2b5d25232e63
Evgeny <transingularity@gmail.com>
2024-06-18 16:00:08 +0300

    style: fixed sqlfluff

--- src/main/resources/db/migration/1.0/V1__init_schema.sql
+++ src/main/resources/db/migration/1.0/V1__init_schema.sql
@@ -2,7 +2,7 @@ drop table if exists state;
 
 create table state
 (
-    id      integer primary key autoincrement,
-    word    varchar(60) not null,
+    id integer primary key autoincrement,
+    word varchar(60) not null,
     created timestamp not null default current_timestamp
 );
--- src/test/resources/db/migration/data/R__init_state_fixtures.sql
+++ src/test/resources/db/migration/data/R__init_state_fixtures.sql
@@ -3,23 +3,23 @@ FROM
     state;
 
 INSERT
-    INTO
-        state(
-            word,
-            created
-        )
-    VALUES(
-        'word1',
-        '2024-01-01 01:01:00'
-    );
+INTO
+state (
+    word,
+    created
+)
+VALUES (
+    'word1',
+    '2024-01-01 01:01:00'
+);
 
 INSERT
-    INTO
-        state(
-            word,
-            created
-        )
-    VALUES(
-        'word2',
-        '2024-01-01 01:01:00'
-    );
+INTO
+state (
+    word,
+    created
+)
+VALUES (
+    'word2',
+    '2024-01-01 01:01:00'
+);
