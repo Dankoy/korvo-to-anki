c4e88a148f82bf67aab763604fee2093dd3445a2
Evgeny <transingularity@gmail.com>
2024-06-18 15:13:21 +0300

    style: style

--- src/main/java/ru/dankoy/korvotoanki/config/datasource/StateDataSourceConfig.java
+++ src/main/java/ru/dankoy/korvotoanki/config/datasource/StateDataSourceConfig.java
@@ -14,7 +14,6 @@ import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
