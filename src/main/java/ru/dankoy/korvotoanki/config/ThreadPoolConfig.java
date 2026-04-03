f4e9a2637f147aca391b32df2e1346a5896d55fa
Evgeny <transingularity@gmail.com>
2026-04-03 09:41:02 +0300

    fix: exit command should close context correctly

--- src/main/java/ru/dankoy/korvotoanki/config/ThreadPoolConfig.java
+++ src/main/java/ru/dankoy/korvotoanki/config/ThreadPoolConfig.java
@@ -2,14 +2,27 @@ package ru.dankoy.korvotoanki.config;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ThreadPoolConfig {
 
-  @Bean(destroyMethod = "shutdownNow")
+  @Bean
   public ExecutorService ankiConverterTaskExecutor() {
-    return Executors.newFixedThreadPool(2);
+    return Executors.newFixedThreadPool(2, Thread.ofVirtual().factory());
+  }
+
+  // This is necessary to gracefully shutdown executor service
+  @Bean
+  public DisposableBean shutdownExecutor(ExecutorService ankiConverterTaskExecutor) {
+    return () -> {
+      ankiConverterTaskExecutor.shutdown();
+      if (!ankiConverterTaskExecutor.awaitTermination(5, TimeUnit.SECONDS)) {
+        ankiConverterTaskExecutor.shutdownNow();
+      }
+    };
   }
 }
